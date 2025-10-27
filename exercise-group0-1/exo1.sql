b8e5e2dee43228103cf0e72045d6dceaba2e4d18
Dan13615 <daniil.stepanov@epitech.eu>
2025-10-27 19:26:51 +0100

    [+] Lab 3 - exo 1

---
+++ exercise-group0-1/exo1.sql
@@ -0,0 +1,31 @@
+use Northwind
+--1. Provide the number of products with prices less than 10 or greater than 20.
+
+SELECT COUNT(*) FROM Products p 
+WHERE p.UnitPrice < 10 OR p.UnitPrice > 20
+
+
+--2. Provide the maximum product price for products priced below 20.
+
+SELECT MAX(unitprice) FROM Products p
+WHERE p.UnitPrice < 20
+
+-- OR
+
+SELECT TOP 1 WITH TIES UnitPrice, ProductName FROM Products p
+WHERE p.UnitPrice < 18.3
+ORDER BY p.UnitPrice desc
+
+
+--3. Provide the maximum, minimum, and average price for products sold in bottles.
+
+SELECT MIN(unitprice), MAX(unitprice), AVG(unitprice) FROM Products p
+WHERE p.Quantityperunit LIKE '%bottle%'
+
+
+--4. List information about all products priced above the average.
+
+
+
+
+--5. Provide the total value of the order with order number 10250.
